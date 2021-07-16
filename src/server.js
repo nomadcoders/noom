d8719f6bbee02bd8937e9f0f89bad7be33e54dca
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-07-16 19:00:38 +0900

    0.4 Recap

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 app.listen(3000, handleListen);
