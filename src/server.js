83105e6afb0045807c26c8460fd71ed308d25fd8
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-07-16 18:54:06 +0900

    0.3 Frontend Setup

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
-console.log("hello");
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
 
-app.listen(3000);
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+app.listen(3000, handleListen);
