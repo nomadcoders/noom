3372e193c73133d6d9404c37287ad9868274ddc6
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-07-19 00:52:27 +0900

    2.8 Room Count part One

--- src/server.js
+++ src/server.js
@@ -13,6 +13,21 @@ app.get("/*", (_, res) => res.redirect("/"));
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
+function publicRooms() {
+  const {
+    sockets: {
+      adapter: { sids, rooms },
+    },
+  } = wsServer;
+  const publicRooms = [];
+  rooms.forEach((_, key) => {
+    if (sids.get(key) === undefined) {
+      publicRooms.push(key);
+    }
+  });
+  return publicRooms;
+}
+
 wsServer.on("connection", (socket) => {
   socket["nickname"] = "Anon";
   socket.onAny((event) => {
