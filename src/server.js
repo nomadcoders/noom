6b19944765df927d2bd963408c66f42d04cae25b
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-07-19 01:21:39 +0900

    2.9 Room Count part Two

--- src/server.js
+++ src/server.js
@@ -37,11 +37,15 @@ wsServer.on("connection", (socket) => {
     socket.join(roomName);
     done();
     socket.to(roomName).emit("welcome", socket.nickname);
+    wsServer.sockets.emit("room_change", publicRooms());
   });
   socket.on("disconnecting", () => {
     socket.rooms.forEach((room) =>
       socket.to(room).emit("bye", socket.nickname)
     );
+  });
+  socket.on("disconnect", () => {
+    wsServer.sockets.emit("room_change", publicRooms());
   });
   socket.on("new_message", (msg, room, done) => {
     socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
