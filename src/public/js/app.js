fc85aa75aa87ae2d128ca1e475c11b1bf947b38d
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-07-18 22:39:24 +0900

    2.2 SocketIO is Amazing

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,15 @@
 const socket = io();
+
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+
+function handleRoomSubmit(event) {
+  event.preventDefault();
+  const input = form.querySelector("input");
+  socket.emit("enter_room", { payload: input.value }, () => {
+    console.log("server is done!");
+  });
+  input.value = "";
+}
+
+form.addEventListener("submit", handleRoomSubmit);
--- src/server.js
+++ src/server.js
@@ -14,7 +14,12 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
-  console.log(socket);
+  socket.on("enter_room", (msg, done) => {
+    console.log(msg);
+    setTimeout(() => {
+      done();
+    }, 10000);
+  });
 });
 
 /*
