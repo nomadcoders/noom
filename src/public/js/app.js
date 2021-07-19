98861d46c60cc211a2a46f051acf2127d103c68a
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-07-20 00:48:34 +0900

    3.8 Senders

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -82,6 +82,13 @@ function handleCameraClick() {
 
 async function handleCameraChange() {
   await getMedia(camerasSelect.value);
+  if (myPeerConnection) {
+    const videoTrack = myStream.getVideoTracks()[0];
+    const videoSender = myPeerConnection
+      .getSenders()
+      .find((sender) => sender.track.kind === "video");
+    videoSender.replaceTrack(videoTrack);
+  }
 }
 
 muteBtn.addEventListener("click", handleMuteClick);
